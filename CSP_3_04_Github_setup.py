4592b8240304c3604e3b3bc51ac14e92701275ce
skestell26 <skestell26@bentleyschool.org>
2025-11-03 08:36:04 -0800

    Add files via upload

--- CSP_3_04_Github_setup.py
+++ CSP_3_04_Github_setup.py
@@ -2,4 +2,4 @@
 #Modify the below function such that it says your name instead.
 #Then reupload it to Github classroom.
 def sayHi():
-    print("Hello my name is Mr Currin.")
+    print("Hello my name is sophie.")
